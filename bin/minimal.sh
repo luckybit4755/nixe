60603c3be844af6d2a7429dda2d77cb52f9ade34
vgvm <luckybit4755@gmail.com>
2022-02-22 14:48:06 -0500

    update "l" and "sl" and add curl instructions

--- bin/minimal.sh
+++ bin/minimal.sh
@@ -1,6 +1,8 @@
+# curl https://luckybit4755.github.io/nixe/bin/minimal.sh > /tmp/minimal.sh ; source /tmp/minimal.sh
 set -o vi
 export EDITOR=vim
-alias l='ls -lathr'
+alias l='ls -Flathr'
 alias md='mkdir -p'
 alias rd='rmdir'
-sl() { if [ $# -lt 1 ] || [ $# -gt 1 ] || [ -d ${1} ]; then ls -F -h ${*} ; else less -Si -r ${*} ; fi }
+alias nosnit='rm -rf snit'
+sl() { [ $# -lt 1 ] || [ $# -gt 1 ] || [ -d ${1} ] && ls -Fthr ${*} || less -Si -r ${*} ; }
