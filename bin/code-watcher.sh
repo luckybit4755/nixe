089a0ec65c94184ebab5c7afc05ffd3227595815
vgvm <luckybit4755@gmail.com>
2022-04-06 10:26:10 -0400

    mac doesn't like the 'date +s -r file' syntax

--- bin/code-watcher.sh
+++ bin/code-watcher.sh
@@ -7,25 +7,25 @@ _code_watcher_main() {
 	local tmp=/tmp/.cw-${USER}-$( pwd | sha256sum  | awk '{print $1}' ).txt
 
 	while true ; do
-        local nu=$( _code_watcher_timestamp )
-        if [ "${nu}" = "${stamp}" ] ; then
-            echo that is boring >/dev/null
-        else
-            stamp=${nu}
+		local nu=$( _code_watcher_timestamp )
+		if [ "${nu}" = "${stamp}" ] ; then
+			echo that is boring >/dev/null
+		else
+			stamp=${nu}
 
 			reset
-            clear
-            echo "update detected at ($(date)), running ${*}"
+			clear
+			echo "update detected at ($(date)), running ${*}"
 			printf "%${COLUMNS}s\n" "" | sed 's, ,-,g'
-            time ${*} 2>&1 | tee ${tmp}
+			time ${*} 2>&1 | tee ${tmp}
 			printf "%${COLUMNS}s\n" "" | sed 's, ,-,g'
-        fi
-        sleep 1
-    done
+		fi
+		sleep 1
+	done
 }
 
 _code_watcher_timestamp() {
-	_code_watcher_find | xargs -n 1 date +'%s' -r 
+	_code_watcher_find | xargs -n 1 date -r
 }
 
 # https://unix.stackexchange.com/questions/15308/how-to-use-find-command-to-search-for-multiple-extensions
