303491e561b9e2da728e8b41071f01efa92b647c
vgvm <luckybit4755@gmail.com>
2022-09-27 12:41:43 -0400

    more robust junk character replacement

--- bin/spacey.sh
+++ bin/spacey.sh
@@ -1,9 +1,9 @@
 #!/bin/bash	
 
 _spacey_main() {
-	ls ${*} | grep ' ' | while read file ; do
-		local space=$( echo ${file} | tr '[ ()]' '[_\-]' )
-		if [ "${line}" != "${space}" ] ; then
+	ls ${*} | while read file ; do
+		local space=$( echo ${file} | sed 's,[^a-zA-Z0-9_.\-],_,g' )
+		if [ "${file}" != "${space}" ] ; then
 			mv -i "${file}" ${space}
 		fi
 	done
