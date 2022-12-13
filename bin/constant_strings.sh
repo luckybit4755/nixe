d0176176b946219c29b55e4ec0759607510f224c
vgvm <luckybit4755@gmail.com>
2022-12-13 16:12:03 -0500

    couple of tweaks

--- bin/constant_strings.sh
+++ bin/constant_strings.sh
@@ -3,22 +3,37 @@
 _constant_strings_main() {
 	cat ${*} | gawk '
 	function constant_string( value ) {
+		first = substr(value,2,1);
+		if ( first ~ /[0-9$.\\]/ ) return; # avoid junk
+		if ( value ~ /[ \t]/ ) return; # no multi-word constants... good idea or bad idea?
+
 		name = value;
 		name = gensub( /([a-z])([A-Z])/, "\\1_\\2", "g", value );
 		name = toupper( substr( name, 2 ) );
+		
+		gsub( "-", "_", name );
 		sub( "^[^a-zA-Z_0-9]*", "", name );
 		sub( "[^a-zA-Z_0-9].*", "", name );
-		if ( 0 == length( name ) ) {
+		if ( 2 >= length( name ) ) {
 			printf( "// %s\n", value );
 		} else {
-			printf( "public final static String %s = %s;\n", name, value );
+			printf( "public final static String %s = %s;\n", name, value, first );
 		}
 	}
-	{
-		len = length( $0 );
+
+	/\/\*/ { COMMENT = 1 }
+
+	!COMMENT {
+		clean = $0;
+		sub( /\/\/.*/, "", clean );
+
+		# very hacky
+		gsub( /'"'"'/, "\"", clean );
+
+		len = length( clean );
 		started = -1;
 		for ( i = 1 ; i <= len ; i++ ) {
-			c = substr( $0, i, 1 );
+			c = substr( clean, i, 1 );
 			if ( "\\" == c ) {
 				i++;
 			} else {
@@ -26,13 +41,18 @@ _constant_strings_main() {
 					if ( -1 == started ) {
 						started = i;
 					} else {
-						constant_string( substr( $0, started, i - started + 1 ) );
+						constant_string( substr( clean, started, i - started + 1 ) );
 						started = -1;
 					}
 				}
 			}
 		}
-	}'
+	
+
+	}
+
+	/\*\// { COMMENT = 0 }
+	' | sort -u
 }
 
 _constant_strings_main ${*}
