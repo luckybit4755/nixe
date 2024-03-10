27a5ebdd8d6bd57f0c55ef86488d07d55fa1ed77
vgvm <luckybit4755+lbl@gmail.com>
2024-03-10 16:41:56 -0400

    improve titles and argument display

--- bin/vim_log.sh
+++ bin/vim_log.sh
@@ -30,20 +30,69 @@ _vim_log_entry() {
 	fi
 }
 
+random_emoji() {
+    #"(\__/)"
+    emojis=(
+        "(ᵔᴥᵔ)"
+        "V•ᴥ•V"
+        "(っ◕‿◕)っ"
+        "ʕ·ᴥ·ʔ"
+		"╰•‿•╯"
+		"(^_^)"
+		"(∩^o^)⊃━☆ﾟ.*"
+		"(o˘◡˘o)"
+		"(づ｡◕‿‿◕｡)づ"
+		"(≧∀≦)"
+		"(◕‿◕✿)"
+		"(^‿^)"
+		"(´･ω･\`)"
+		"(づ￣ ³￣)づ"
+		"(◕‿◕✿)"
+		"✿◡‿◡"
+		"(◕દ◕)"
+		"(●'◡'●)シ ︎"
+		"૮₍ ´• ˕ • ₎ა"
+		"( ˘ ³˘)♥︎"
+		"(• ◡•)" 
+		"(❍ᴥ❍ʋ)"
+		"(｡◕‿‿◕｡)"
+		"(o˘◡˘o)"
+		"(∩^o^)⊃━☆ﾟ.*"
+		"ˁ(⦿ᨎ⦿)ˀ"
+		" Ƹ̵̡Ӝ̵̨̄Ʒ"
+    )
+
+    # Get the number of emojis in the array
+    num_emojis=${#emojis[@]}
+
+    # Generate a random index between 0 and num_emojis-1
+    random_index=$((RANDOM % num_emojis))
+
+    # Return the randomly selected emoji
+    echo "${emojis[random_index]}"
+}
+
 
 # no main to maintain the ${*} as much as possible...
 export KEY=$( uuidgen 2>/dev/null || echo r${RANDOM}-${RANDOM}-${RANDOM}-${RANDOM} )
 export FULL=$( _vim_log_fullpath ${*} )
+export VIM_TMI=${VIM_TMI-$(random_emoji)}
+
+args=""
+for arg in ${*} ; do
+	if [ -f ${arg} ] ; then
+		arg="${arg}"
+	else
+		if [[ ${arg} = *: ]] ; then
+			file=$( echo ${arg} | cut -d: -f1)
+			line=$( echo ${arg} | cut -d: -f2)
+			arg="${file} +${line}"
+		fi
+	fi
+	args="${args} ${arg}"
+done
 
 _vim_log_entry start
-vim ${*}
+vim ${args}
 _vim_log_entry stop
-
-
-#echo -e "${key}\t${HOSTNAME}\t$( _vim_log_timestamp )\tstart\t"${*} >> ${VIM_LOG}
-#_vim_log_set_title vim $( basename ${*} 2>/dev/null )
-#
-#vim ${*}
-#
-#echo -e "${key}\t${HOSTNAME}\t$( _vim_log_timestamp )\tstop\t"${*}  >> ${VIM_LOG}
-#_vim_log_set_title $( basename ${PWD} 2>/dev/null )
+_vim_log_set_title $(random_emoji)
