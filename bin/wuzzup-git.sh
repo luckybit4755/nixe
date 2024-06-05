d3a8a4cc2aa73919afa189b42a8a2e57dab424bf
vgvm <luckybit4755+lbl@gmail.com>
2024-06-05 08:39:49 -0400

    lotta fixes

--- bin/wuzzup-git.sh
+++ bin/wuzzup-git.sh
@@ -37,14 +37,13 @@ _wuzzup_git_main() {
 					return
 				fi
 				let i=${i}+1 ; local repo=${args[${i}]}
-				#let i=${i}+1 ; local ticket=${args[${i}]}
 				let i=${i}+1 ; local name=${args[${i}]}
-				#_wuzzup_git_new_task ${repo} ${ticket} ${name}
 				_wuzzup_git_new_task ${repo} ${name}
 				;;
 			-lmod) _wuzzup_git_modified 1 ;;
 			-mod)  _wuzzup_git_modified 0 ;;
 			-push-back|-push|-pb) _wuzzup_git_push_back ;;
+			-catch-up|-cu) _wuzzup_git_catch_up ;;
 			-remote) 
 				let n=${i}+1
 				if [ ${n} -ge ${#} ] ; then 
@@ -97,9 +96,8 @@ _wuzzup_git_usage_nt() {
 cat << EOM
   -nt
   -newtask
-  -new-task <REPO> <TICKET> <NAME> 
+  -new-task <REPO> <NAME> 
   clone repository, create a local branch from origin/${MAIN}, and a corresponding remote branch
-  <TICKET> should be like the JIRA ID (XXX-1234)
   <NAME>   should be short like: super-cool-thing-u-love
 EOM
 }
@@ -197,26 +195,34 @@ _wuzzup_git_columns() {
 _wuzzup_git_clone_branch() {
 	local repo=${1}; shift
 	local branch=${1}; shift
-	local directory=$( echo ${branch} | sed 's,/,_,g' )
+	local last=$(echo ${repo} | sed 's,.*/,,;s,\.git$,,')
+	local directory=${last}-$( echo ${branch} | sed 's,/,_,g' )
 	echo git clone -b ${branch} --single-branch ${repo} ${directory}
+	echo hit enter
+	read
 	git clone -b ${branch} --single-branch ${repo} ${directory}
 	#echo consider running: git push ${repo} ${branch}:${branch}
 }
 
 _wuzzup_git_new_task() {
 	local repo=${1}; shift
-	#local ticket=${1}; shift
 	local name=${1}; shift
 
-	local ws="${ticket}_${name}"
-	ws="${name}"
+	local last=$(echo ${repo} | sed 's,.*/,,;s,\.git$,,')
+	local ws="${last}-${name}"
 	if [ -d ${ws} ] ; then
 		echo ${ws} already exists
 		return 1
 	fi
 
-	local branch="${ticket}/${name}"
-	branch="${name}"
+	local branch="${name}"
+
+	echo -n "do you want to: create a branch called '${branch} on ${repo}/${MAIN} [y/N]? "
+	read r
+	if [ "y" != "${r}" ] ; then
+		echo "I don't blame you..."
+		return 0
+	fi
 
 	echo cloning to ${ws}
 	git clone ${repo} ${ws} || return 2
@@ -261,6 +267,27 @@ _wuzzup_git_push_back() {
 		echo --------------------------------------------
 		echo git push origin ${branch}
 		git push origin ${branch}
+		echo --------------------------------------------
+		echo "check $( _wuzzup_git_file_to_url . | sed 's,\.$,,' )"
+	else
+		echo "ok, maybe later then"
+	fi
+}
+
+_wuzzup_git_catch_up() {
+	local branch=$( git status | head -1 | sed 's,.* ,,' )
+
+	echo "remote origin set to:"
+	git remote -v | grep origin
+	echo 
+	echo -n "do you want catch up to to date with the ${MAIN} branch [y/N]? "
+	read r
+	if [ "y" = "${r}" ] ; then
+		echo --------------------------------------------
+		echo git fetch origin ${MAIN}
+		echo git merge origin/${MAIN}
+		git fetch origin ${MAIN} || return ${?}
+		git merge origin/${MAIN} || return ${?}
 		echo --------------------------------------------
 		echo "check $( _wuzzup_git_file_to_url . | sed 's,\.$,,' )"
 	else
